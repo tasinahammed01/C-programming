abfaeb80811768aadcc06b096a0fa405459d01d2
tasinahammed01 <tanjid.ahammed.01@gmail.com>
2025-07-24 01:53:36 +0600

    array syntex learned

---
+++ Array_In_C/Array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int arr[5];
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("enter element number:%d\n", i);
+
+        scanf("%d", &arr[i]);
+    }
+    printf("%d", arr[1]);
+}
